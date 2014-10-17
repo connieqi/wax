b5339a7f5cf398c54db83d8c2e84fd54c43813ad
Connie Qi <connieqi@umich.edu>
2014-10-17 05:15:01 -0400

    added uri parsing

--- js/wax.js
+++ js/wax.js
@@ -1,6 +1,8 @@
-
 var waxWane = {
     wax: function() {
+        $('#masthead-positioner').hide();
+        $('#masthead-positioner-height-offset').hide();
+
         $('#msg').text('Video waxed');
         $('#wax-btn').hide();
         $('#wane-btn').show();
@@ -10,11 +12,34 @@ var waxWane = {
         $('#msg').text('Video waned');
         $('#wane-btn').hide();
         $('#wax-btn').show();
+    },
+
+    onYouTube: function(cb) {
+        chrome.tabs.query({'active': true, 'lastFocusedWindow': true},
+        function(tabs) {
+            var url = tabs[0].url;
+
+            // js URI parsing thanks to https://gist.github.com/jlong/2428561
+            var parser = document.createElement('a');
+            parser.href = url;
+
+            if (parser.hostname == 'www.youtube.com' && parser.pathname == '/watch') {
+                cb(true);
+            } else {
+                cb(false);
+            }
+        });
     }
 };
 
 document.addEventListener('DOMContentLoaded', function() {
     console.log('hi wax');
+    waxWane.onYouTube(function(response) {
+        if (response === true) {
+            waxwane.wax();
+        }
+    });
+
     waxWane.wax();
 
     $('#wax-btn').on('click', function() {
